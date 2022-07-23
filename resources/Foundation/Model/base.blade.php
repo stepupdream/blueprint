053b15261abb4eb05ccbb22ce99777719086f3c7
stepupdream <stepupdream1002@gmail.com>
2022-07-23 15:36:15 +0900

    [add] add blade template.

---
+++ resources/Foundation/Model/base.blade.php
@@ -0,0 +1,21 @@
+@php
+use Illuminate\Support\Str;
+echo '<?php' . PHP_EOL;
+@endphp
+
+namespace {{ $namespace }};
+
+@if (!empty($use_extends_class))
+{{ $use_extends_class }}
+
+@else
+@endif
+/**
+ * Class {{ Str::studly($class_name) }}
+ *
+ * {{ blade_phpdoc_support($namespace, 'package') }}
+ */
+abstract class {{ Str::studly($class_name) }}{{ $extends_class_name }}
+{
+    //
+}
