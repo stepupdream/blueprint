5fe95c89e61d6e5d689fd0b4b52f8892395cec01
stepupdream <stepupdream1002@gmail.com>
2022-08-12 20:22:17 +0900

    [add] add  migration creator test code.

--- tests/FoundationCreate/Console/SampleTest.php
+++ tests/FoundationCreate/Console/SampleTest.php
@@ -14,17 +14,17 @@ class SampleTest extends TestCase
      * Laravel's [this-> option ('target')] hindered the test, so test only where it's needed.
      *
      * @test
-     * @doesNotPerformAssertions
      */
     public function foundationCreateCommand(): void
     {
         // config mock
         $configPath = __DIR__.'/../Config.php';
         $configMock = require $configPath;
-        Config::set('stepupdream.blueprint', $configMock);
+        Config::set('stepupdream.blueprint.foundations', $configMock['foundations']);
 
         /* @see FoundationCreateCommand::foundationsConfig() */
         $foundationCreateCommand = new FoundationCreateCommand();
-        $this->executePrivateFunction($foundationCreateCommand, 'foundationsConfig');
+        $result = $this->executePrivateFunction($foundationCreateCommand, 'foundationsConfig');
+        self::assertSame($result, $configMock['foundations']);
     }
 }
--- tests/FoundationCreate/Supports/File/FileOperationTest.php
+++ tests/FoundationCreate/Supports/File/FileOperationTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace StepUpDream\Blueprint\Test\FoundationCreate\Supports\File;
 
+use StepUpDream\Blueprint\Creator\Supports\File\ColumnVersionMigration;
 use StepUpDream\Blueprint\Creator\Supports\File\FileOperation;
 use StepUpDream\Blueprint\Test\TestCase;
 
@@ -12,10 +13,32 @@ class FileOperationTest extends TestCase
     /**
      * @test
      */
+    public function getColumnVersionByTable(): void
+    {
+        $migrationDirectoryPath = __DIR__.'/../../../Migration/Mock/Migration';
+        $fileOperation = $this->app->make(FileOperation::class);
+        $columnVersion = $fileOperation->getColumnVersionByTable($migrationDirectoryPath, 'samples');
+
+        // MockData
+        $columnVersionMigration = new ColumnVersionMigration();
+        $attributes = [];
+        $attributes['name'] = '1_0_1_0';
+        $attributes['email'] = '1_0_0_0';
+        $attributes['email_verified_at'] = '1_0_0_0';
+        $attributes['password'] = '1_0_0_0';
+        $attributes['name2'] = '1_0_1_0';
+        $columnVersionMigration->addColumnVersions($attributes);
+
+        self::assertEquals($columnVersionMigration, $columnVersion);
+    }
+
+    /**
+     * @test
+     */
     public function addTabSpace(): void
     {
-        $creator = $this->app->make(FileOperation::class);
-        $space = $creator->addTabSpace(2);
+        $fileOperation = $this->app->make(FileOperation::class);
+        $space = $fileOperation->addTabSpace(2);
         $testResult = '        ';
 
         self::assertSame($space, $testResult);
@@ -30,12 +53,12 @@ class FileOperationTest extends TestCase
         $outputDirectoryPath = __DIR__.'/../TestFiles/Sample';
         $testFilePath = __DIR__.'/../TestFiles/sample3.yml';
 
-        $creator = $this->app->make(FileOperation::class);
-        $creator->createFile("aaa\n", $outputPath);
+        $fileOperation = $this->app->make(FileOperation::class);
+        $fileOperation->createFile("aaa\n", $outputPath);
         self::assertFileEquals($outputPath, $testFilePath);
 
-        $creator = $this->app->make(FileOperation::class);
-        $creator->createFile("aaa\n", $outputPath, true);
+        $fileOperation = $this->app->make(FileOperation::class);
+        $fileOperation->createFile("aaa\n", $outputPath, true);
         self::assertFileEquals($outputPath, $testFilePath);
 
         if (is_file($outputPath)) {
